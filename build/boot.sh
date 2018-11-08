f31a46c64e30f41f16b66195010b8d7c15bbf6b5
Stefaan Ponnet <stefaan@ponnet.com>
2018-11-08 20:29:26 +0100

    fix: update entrypoint script

--- build/boot.sh
+++ build/boot.sh
@@ -2,7 +2,7 @@
 node /usr/share/admin-server/server.js &
 while true
 do
-    geth --datadir /goerli --rpc --rpccorsdomain "*" --rpcaddr="0.0.0.0" --rpcvhosts "*" --ws --wsaddr="0.0.0.0" --wsorigins="*" --networkid=6284 --password /usr/src/admin-server/data/password --keystore /usr/src/admin-server/data/keystore $EXTRA_OPTS
+    geth --datadir /goerli --rpc --rpccorsdomain "*" --rpcaddr="0.0.0.0" --rpcvhosts "*" --ws --wsaddr="0.0.0.0" --wsorigins="*" --networkid=6284 --password  /usr/share/admin-server/data/password --keystore /usr/src/admin-server/data/keystore $EXTRA_OPTS
     sleep 5
 done
 
