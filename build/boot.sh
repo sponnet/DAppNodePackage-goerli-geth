0727ed8871716245e3955b136bc3cee210df4ad1
Stefaan Ponnet <stefaan@ponnet.com>
2018-11-08 20:19:08 +0100

    fix: added boot.sh shell script

---
+++ build/boot.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+node /usr/share/admin-server/server.js &
+while true
+do
+    geth --datadir /goerli --rpc --rpccorsdomain "*" --rpcaddr="0.0.0.0" --rpcvhosts "*" --ws --wsaddr="0.0.0.0" --wsorigins="*" --networkid=6284 --password /usr/src/admin-server/data/password --keystore /usr/src/admin-server/data/keystore $EXTRA_OPTS
+    sleep 5
+done
+
