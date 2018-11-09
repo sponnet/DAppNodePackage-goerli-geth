674401df114c2af0dff57ad17b4440d3f99c18d9
Stefaan Ponnet <stefaan@ponnet.com>
2018-11-09 10:01:59 +0100

    corrected the path of the keystore

--- build/boot.sh
+++ build/boot.sh
@@ -2,7 +2,7 @@
 node /usr/share/admin-server/server.js &
 while true
 do
-    geth --datadir /goerli --rpc --rpccorsdomain "*" --rpcaddr="0.0.0.0" --rpcvhosts "*" --ws --wsaddr="0.0.0.0" --wsorigins="*" --networkid=6284 --password  /usr/share/admin-server/data/password --keystore /usr/src/admin-server/data/keystore $EXTRA_OPTS
+    geth --datadir /goerli --rpc --rpccorsdomain "*" --rpcaddr="0.0.0.0" --rpcvhosts "*" --ws --wsaddr="0.0.0.0" --wsorigins="*" --networkid=6284 --password  /usr/share/admin-server/data/password --keystore /usr/share/admin-server/data/keystore --mine --unlock 0x68453de7fbb3605186ccf435c08512c2fa9591ef --$EXTRA_OPTS
     sleep 5
 done
 
